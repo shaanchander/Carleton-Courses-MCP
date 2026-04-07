96b3d14130ca14ed259081540e540e943b31fe2a
Shaan Chander <66710968+shaanchander@users.noreply.github.com>
2026-04-07 00:42:16 -0400

    Basic implementation for course_search

--- objects.py
+++ objects.py
@@ -1,12 +1,12 @@
 # TODO:
-# - Add prerequisites, credits
+# - Add prerequisites, credits, instructor
 
 class course:
-    def __init__(self, subject: str, code: str, title: str, description: str):
+    def __init__(self, subject: str, code: str, title: str):
         self.subject = subject
         self.code = code
         self.title = title
-        self.description = description
+        # self.description = description
 
     def __str__(self):
-        return f"{self.subject}{self.code}: {self.title}\n{self.description}"
+        return f"{self.subject}{self.code}: {self.title}"
