8c01f9840d13e5bd75a13a4b1e01a2e5a9987239
Shaan Chander <shaan.chander06@gmail.com>
2026-04-07 15:48:15 -0400

    Implemented course_details helper and added tool to call it

--- objects.py
+++ objects.py
@@ -1,13 +1,9 @@
-# TODO:
-# - Add prerequisites, description
-
 class course:
-    def __init__(self, subject: str, code: str, title: str, start_date: str = "", end_date: str = "", meeting_days: list[str] = [], meeting_times: list[str] = [], credits: int = 0, instructor: str = "Unknown", session_type: str = "", crn: int = 0, section: str = "", status: str = ""):
+    def __init__(self, subject: str, code: str, title: str, start_date: str = "", end_date: str = "", meeting_days: list[str] = [], meeting_times: list[str] = [], credits: int = 0, instructor: str = "Unknown", session_type: str = "", crn: int = 0, section: str = "", section_information: str = "", status: str = ""):
         self.crn = crn
         self.subject = subject
         self.code = code
         self.title = title
-        # self.description = description
         self.start_date = start_date
         self.end_date = end_date
         self.meeting_days = meeting_days
@@ -16,6 +12,7 @@ class course:
         self.instructor = instructor
         self.session_type = session_type # lecture, lab, tutorial, etc.
         self.section = section # A, B, C, etc.
+        self.section_information = section_information
         self.status = status # open, closed, waitlist, etc.
 
     def __str__(self):
